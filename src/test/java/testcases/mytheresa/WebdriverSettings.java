6a89d42bd2e90558701c893861a03405821a76f1
Siddartha Pradhan <siddh010@gmail.com>
2024-04-10 19:54:05 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,8 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe File PATH");
-		WebDriver driver = new ChromeDriver();
+ ChromeOptions capabilities = new ChromeOptions();
+		capabilities.addArguments("--ignore-ssl-errors=yes");
+		capabilities.addArguments("--ignore-certificate-errors");
+		capabilities.addArguments("--allow-running-insecure-content");
+		capabilities.addArguments("--remote-allow-origins=*");
+		capabilities.setAcceptInsecureCerts(true);
+   		WebDriver driver = new RemoteWebDriver(new URL("http://143.199.99.240:4444/wd/hub"), capabilities);
+		
 		return driver;
 	}
 
